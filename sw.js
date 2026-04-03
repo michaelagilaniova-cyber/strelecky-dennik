89d66d8188394e0ee2cada99bf3fa481573b0d8b
michaelagilaniova-cyber <michaela.gilaniova@gmail.com>
2026-04-03 09:36:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'score-offline-v24';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com/3.4.3',
+  'https://unpkg.com/react@18.3.1/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.3.1/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone@7.24.4/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      // Agresívne sťahovanie - ak jeden súbor zlyhá, nezruší sa inštalácia ostatných
+      return Promise.allSettled(
+        urlsToCache.map(url => cache.add(url).catch(err => console.log('Chyba ukladania:', url)))
+      );
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+  // Vyčistenie starého balastu z predchádzajúcich verzií
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return res;
+      }).catch(() => {
+        // Ak zlyhá internet, presmeruje ťa vždy na domácu offline obrazovku
+        if (event.request.mode === 'navigate') return caches.match('/index.html');
+      });
+    })
+  );
+});
